a4a76d7c3ba893f9efb17a3d712512bf6d0781fd
Phill Conrad <pconrad@cs.ucsb.edu>
2018-08-12 14:54:23 -0700

    try with zip file containing IntList.java at top

---
+++ grade.sh
@@ -0,0 +1,34 @@
+
+
+base=/autograder
+if [ "$#" -eq 1 ]
+then
+   base=$1
+fi
+
+# Now ${base}/STUDENT-WORK should contain the unzipped student submission
+# Current directory is the contents of the instructor repo
+# Do whatever you need to do to grade the submission.
+
+# That might mean copying the instructor's Ant build.xml or Maven pom.xml over to the student one,
+# plus copying the contents of ./src/test/java over to ${base}/STUDENT-WORK/src/test/java 
+# And then running there.
+
+# Or it might mean copying the students code into the instructors's git repo (this directory) and
+# running here.
+
+if [ ! -f ${base}/STUDENT-WORK/IntList.java ];
+then
+    echo '{ "score": 0.0,"output": "ERROR: expected exactly one zip file containing IntList.java"}' >  ${base}/results/results.json
+else
+    mkdir -p src/main/java/com/gradescope/intlist/
+    cp ${base}/STUDENT-WORK/IntList.java src/main/java/com/gradescope/intlist/
+fi
+
+
+# Ultimately you want to write to ${base}/results/results.json
+
+mvn -q compile
+mvn -q test
+mvn -q exec:java > ${base}/results/results.json
+
