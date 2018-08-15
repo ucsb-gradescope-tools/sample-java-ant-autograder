d294d8d8486448ad9373aa5710b5a2d5fb9ac68c
Phill Conrad <pconrad@cs.ucsb.edu>
2018-08-14 17:29:26 -0700

    also install ant, not just maven

--- setup.sh
+++ setup.sh
@@ -1,26 +1,12 @@
-apt-get -y install openjdk-8-jdk
-
-echo "******** I N S T A L L I N G      M A V E N  ***************"
-echo "******** I N S T A L L I N G      M A V E N  ***************"
-echo "******** I N S T A L L I N G      M A V E N  ***************"
-echo "******** I N S T A L L I N G      M A V E N  ***************"
-echo "******** I N S T A L L I N G      M A V E N  ***************"
-echo "apt-get -y install maven is the next command..."
-
 apt-get -y update
+apt-get -y install openjdk-8-jdk
 apt-get -y install maven
-
-echo "******** I N S T A L L E D      M A V E N  ***************"
-echo "******** I N S T A L L E D      M A V E N  ***************"
-echo "******** I N S T A L L E D      M A V E N  ***************"
-echo "******** I N S T A L L E D      M A V E N  ***************"
-echo "******** I N S T A L L E D      M A V E N  ***************"
-
-
+apt-get -y install ant
 apt-get -y install bash
 
 # The following lines are to make sure we can install packages from maven
 # See https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/1396760
 # and https://github.com/docker-library/openjdk/issues/19#issuecomment-70546872
-apt-get install --reinstall ca-certificates-java
+
+apt-get -y install --reinstall ca-certificates-java
 update-ca-certificates -f
