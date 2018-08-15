4b7a17481574ac5d70ff849116ca09bfb3e67fab
Phill Conrad <pconrad@cs.ucsb.edu>
2018-08-14 18:17:58 -0700

    we can haz results.json, but we need to fix the scripts to generate and put in right place

--- src/RationalTest.java
+++ src/RationalTest.java
@@ -27,119 +27,139 @@ public class RationalTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    @GradedTest(name="test with a zero denominator", max_score=1)
+    @GradedTest(name="test with a zero denominator", max_score=10)
     public void test_denom_zero() {
 		Rational r = new Rational(1,0);
     }
     
     @Test
-	@GradedTest(max_score=1)
+	@GradedTest(name="test_getNumerator_20_25",max_score=10)
     public void test_getNumerator_20_25() {
 		assertEquals(4, r_20_25.getNumerator());
     }
     
     @Test
-    public void test_getNumerator_13_4() {
+	@GradedTest(name="getNumerator_13_4",max_score=10)
+	public void test_getNumerator_13_4() {
 		assertEquals(13, r_13_4.getNumerator());
     }
     
     @Test
-    public void test_getDenominator_20_25() {
+	@GradedTest(name="getDenominator_20_25",max_score=10)
+	public void test_getDenominator_20_25() {
 		assertEquals(5, r_20_25.getDenominator());
     }
     
     @Test
-    public void test_getDenominator_13_4() {
+	@GradedTest(name="getDenominator_13_4",max_score=10)
+	public void test_getDenominator_13_4() {
 		assertEquals(4, r_13_4.getDenominator());
     }
 
     @Test
-    public void test_toString_5_15() {
+	@GradedTest(name="toString_5_15",max_score=10)
+	public void test_toString_5_15() {
 		assertEquals("1/3",r_5_15.toString());
     }
 
     @Test
-    public void test_toString_24_6() {
+	@GradedTest(name="toString_24_6",max_score=10)
+	public void test_toString_24_6() {
 		assertEquals("4",r_24_6.toString());
     }
 
     @Test
-    public void test_toString_3_7() {
+	@GradedTest(name="toString_3_7",max_score=10)
+	public void test_toString_3_7() {
 		assertEquals("3/7",r_3_7.toString());
     }
     
     @Test
-    public void test_toString_20_25() {
+	@GradedTest(name="toString_20_25",max_score=10)
+	public void test_toString_20_25() {
 		assertEquals("4/5",r_20_25.toString());
     }
 
     @Test
-    public void test_toString_0_1() {
+	@GradedTest(name="toString_0_1",max_score=10)
+	public void test_toString_0_1() {
 		assertEquals("0",r_0_1.toString());
     }
 
     @Test
-    public void test_gcd_5_15() {
+	@GradedTest(name="gcd_5_15",max_score=10)
+	public void test_gcd_5_15() {
 		assertEquals(5, Rational.gcd(5,15));
     }
 
     @Test
-    public void test_gcd_15_5() {
+	@GradedTest(name="gcd_15_5",max_score=10)
+	public void test_gcd_15_5() {
 		assertEquals(5, Rational.gcd(15,5));
     }
 
     @Test
-    public void test_gcd_24_6() {
+	@GradedTest(name="gcd_24_6",max_score=10)
+	public void test_gcd_24_6() {
 		assertEquals(6, Rational.gcd(24,6));
     }
 
     @Test
-    public void test_gcd_17_5() {
+	@GradedTest(name="gcd_17_5",max_score=10)
+	public void test_gcd_17_5() {
 		assertEquals(1, Rational.gcd(17,5));
     }
     
     @Test
-    public void test_gcd_1_1() {
+	@GradedTest(name="gcd_1_1",max_score=10)
+	public void test_gcd_1_1() {
 		assertEquals(1, Rational.gcd(1,1));
     }
 
     @Test
-    public void test_gcd_20_25() {
+	@GradedTest(name="gcd_20_25",max_score=10)
+	public void test_gcd_20_25() {
 		assertEquals(5, Rational.gcd(20,25));
     }
 
     @Test
-    public void test_rational_m10_m5() {
+	@GradedTest(name="rational_m10_m5",max_score=10)
+	public void test_rational_m10_m5() {
 		Rational r = new Rational(-10,-5);
 		assertEquals("2",r.toString());
     }
 
     @Test
-    public void test_rational_m5_6() {
+	@GradedTest(name="rational_m5_6",max_score=10)
+	public void test_rational_m5_6() {
 		Rational r = new Rational(-5,6);
 		assertEquals("-5/6",r.toString());
     }
 	
     @Test
-    public void test_rational_7_m8() {
+	@GradedTest(name="rational_7_m8",max_score=10)
+	public void test_rational_7_m8() {
 		Rational r = new Rational(7,-8);
 		assertEquals("-7/8",r.toString());
     }
 	
     @Test
-    public void test_r_5_15_times_r_3_7() {
+	@GradedTest(name="r_5_15_times_r_3_7",max_score=10)
+	public void test_r_5_15_times_r_3_7() {
 		Rational r = r_5_15.times(r_3_7);
 		assertEquals("1/7",r.toString());
     }
 	
     @Test
-    public void test_r_3_7_times_r_13_4() {
+	@GradedTest(name="r_3_7_times_r_13_4",max_score=10)
+	public void test_r_3_7_times_r_13_4() {
 		Rational r = r_3_7.times(r_13_4);
 		assertEquals("39/28",r.toString());
     }
 	
     @Test
-    public void test_r_m3_1_times_1_m3() {
+	@GradedTest(name="r_m3_1_times_1_m3",max_score=10)
+	public void test_r_m3_1_times_1_m3() {
 		Rational r_m3_1 = new Rational(-3,1);
 		Rational r_1_m3 = new Rational(1,-3);
 		Rational r = r_m3_1.times(r_1_m3);
@@ -147,22 +167,26 @@ public class RationalTest {
     }
 	
 	@Test
-    public void test_product_of_r_5_15_and_r_3_7() {
+	@GradedTest(name="product_of_r_5_15_and_r_3_7",max_score=10)
+	public void test_product_of_r_5_15_and_r_3_7() {
 		Rational r = Rational.product(r_5_15,r_3_7);
 		assertEquals("1/7",r.toString());
     }
 	
     @Test
-    public void test_product_of_r_3_7_and_r_13_4() {
+	@GradedTest(name="product_of_r_3_7_and_r_13_4",max_score=10)
+	public void test_product_of_r_3_7_and_r_13_4() {
 		Rational r = Rational.product(r_3_7,r_13_4);
 		assertEquals("39/28",r.toString());
     }
 	
     @Test
-    public void test_product_of_r_m3_1_and_1_m3() {
+	@GradedTest(name="product_of_r_m3_1_and_1_m3",max_score=10)
+	public void test_product_of_r_m3_1_and_1_m3() {
 		Rational r_m3_1 = new Rational(-3,1);
 		Rational r_1_m3 = new Rational(1,-3);
 		Rational r = Rational.product(r_m3_1,r_1_m3);
 		assertEquals("1",r.toString());
     }
+	
 }
