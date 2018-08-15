b7fffb988e22b7b91262a2af014245e034cceb19
Phill Conrad <pconrad@cs.ucsb.edu>
2018-08-14 17:49:12 -0700

    WIP PC/NP  at least it compiles with Ant

--- src/RunTests.java
+++ src/RunTests.java
@@ -1,18 +1,14 @@
-package com.gradescope.intlist.tests;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+
 // import junit.tests.framework.TestListenerTest
-import com.gradescope.intlist.tests.IntListTest;
+
 import com.gradescope.jh61b.grader.GradedTestListenerJSON;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({
-        IntListTest.class,
-            IntListPredicates.class,
-            })
+@Suite.SuiteClasses({RationalTest.class})
 public class RunTests {
     public static void main(String[] args) {
         JUnitCore runner = new JUnitCore();
